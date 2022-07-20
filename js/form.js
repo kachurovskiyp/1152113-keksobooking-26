09d4b1d1c9fb4082be803e6a7479738c778a69b9
Pavlo <kachurovskiy.p@gmail.com>
2022-07-20 16:58:04 +0200

    Добавляет валидацию

--- js/form.js
+++ js/form.js
@@ -4,6 +4,37 @@ const formFieldsets = form.querySelectorAll('fieldset');
 const mapFiltersSelects = mapFilters.querySelectorAll('select');
 const mapFiltersFieldsets = mapFilters.querySelectorAll('fieldset');
 
+const disableForm = () => {
+  form.classList.add('ad-form--disabled');
+  formFieldsets.forEach((fieldset) => {
+    fieldset.setAttribute('disabled', 'disabled');
+  });
+
+  mapFilters.classList.add('map__filters--disabled');
+  mapFiltersSelects.forEach((select) => {
+    select.setAttribute('disabled', 'disabled');
+  });
+
+  mapFiltersFieldsets.forEach((fieldset) => {
+    fieldset.setAttribute('disabled', 'disabled');
+  });
+};
+
+const enableForm = () => {
+  form.classList.remove('ad-form--disabled');
+  formFieldsets.forEach((fieldset) => {
+    fieldset.disabled = false;
+  });
+
+  mapFilters.classList.remove('map__filters--disabled');
+  mapFiltersSelects.forEach((select) => {
+    select.disabled = false;
+  });
+
+  mapFiltersFieldsets.forEach((fieldset) => {
+    fieldset.disabled = false;
+  });
+};
 
 // Form validation
 
@@ -117,4 +148,5 @@ roomSelect.addEventListener('change', () => {
 });
 
 onRoomChange(roomSelect.value);
+
 export {disableForm, enableForm};
