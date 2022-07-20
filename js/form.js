d473ba5fb75ebc5bd33918753f3725aa22f31dde
Pavlo <kachurovskiy.p@gmail.com>
2022-07-20 16:35:55 +0200

    Правит валидацию

--- js/form.js
+++ js/form.js
@@ -36,27 +36,117 @@ const enableForm = () => {
   });
 };
 
-// validation
+// Form validation
 
-const errorMessage = document.createElement('p');
-errorMessage.classList.add('ad-form__error');
-form.querySelector('.ad-form__element--submit').append(errorMessage);
+const defaultConfig = {
+   classTo: 'ad-form',
+   errorClass: 'has-danger',
+   successClass: 'has-success',
+   errorTextParent: 'my-error',
+   errorTextTag: 'p',
+   errorTextClass: 'text-help'
+};
 
+const removeErrors = () => {
+  const errors = form.querySelectorAll('.validation-error');
+  if(errors) {
+    errors.forEach((error) => {error.remove()});
+  }
+}
+
+const pristine = new Pristine(form, defaultConfig);
 const titleInput = form.querySelector('#title');
+const priceInput = form.querySelector('#price');
 
-const pristine = new Pristine(form);
+pristine.addValidator(titleInput, () => {
+  if(titleInput.value.length < 30) {
+    return false;
+  }
+  return true;
+}, 'Заголовок должен содержать минимум 30 знаков');
 
-pristine.addValidator(
-  titleInput,
-  () => {
-  console.log('add');
-  return false;
-});
+pristine.addValidator(priceInput, () => {
+  if(priceInput.value * 1 > 100000) {
+    return false;
+  }
+  return true;
+}, 'Цена не может превыщать 100 000');
 
-form.addEventListener('submit', function (evt) {
+form.addEventListener('submit', (evt) => {
   evt.preventDefault();
-  const isValid = pristine.validate();
-  console.log(isValid);
+
+  const isValide = pristine.validate();
+
+  if(!isValide){
+    removeErrors();
+    pristine.getErrors().forEach((error) => {
+
+      const errorMessage = document.createElement('span');
+      errorMessage.classList.add('validation-error');
+      errorMessage.textContent = error.errors;
+
+      error.input.parentNode.append(errorMessage);
+    });
+  } else {
+    removeErrors();
+  }
 });
+
+const roomSelect = form.querySelector('#room_number');
+const capacitySelect = form.querySelector('#capacity');
+
+const onRoomChange = (value) => {
+  capacitySelect.querySelectorAll('option').forEach((option) => {
+      option.removeAttribute('disabled');
+  });
+
+  switch(value) {
+    case '1': {
+      capacitySelect.querySelector('option[value="1"]').setAttribute('selected', 'selected');
+      capacitySelect.querySelectorAll('option').forEach((option) => {
+        if(option.value != 1){
+          option.setAttribute('disabled', 'disabled');
+        }
+      });
+      break;
+    }
+
+    case '2': {
+      capacitySelect.querySelector('option[value="2"]').setAttribute('selected', 'selected');
+      capacitySelect.querySelectorAll('option').forEach((option) => {
+        if(option.value > value || option.value == 0){
+          option.setAttribute('disabled', 'disabled');
+        }
+      });
+      break;
+    }
+
+    case '3': {
+      capacitySelect.querySelector('option[value="3"]').setAttribute('selected', 'selected');
+      capacitySelect.querySelectorAll('option').forEach((option) => {
+        if(option.value > value || option.value == 0) {
+          option.setAttribute('disabled', 'disabled');
+        }
+      });
+      break;
+    }
+
+    case '100': {
+      capacitySelect.querySelector('option[value="0"]').setAttribute('selected', 'selected');
+      capacitySelect.querySelectorAll('option').forEach((option) => {
+        if(option.value != 0){
+          option.setAttribute('disabled', 'disabled');
+        }
+      });
+      break;
+    }
+  }
+}
+
+roomSelect.addEventListener('change', () => {
+  onRoomChange(roomSelect.value);
+});
+
+onRoomChange(roomSelect.value);
 
 export {disableForm, enableForm};
