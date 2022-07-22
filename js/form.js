78cb10b53bd84b4824dc11cdc44775ca72c328a8
Pavlo <kachurovskiy.p@gmail.com>
2022-07-22 21:48:44 +0200

    Правит ошибки линтера

--- js/form.js
+++ js/form.js
@@ -61,36 +61,32 @@ const onRoomChange = (value) => {
     case '1': {
       const pristine = new Pristine(form, pristineConfig);
       capacitySelect.querySelector('option[value="1"]').setAttribute('selected', 'selected');
-      pristine.addValidator(capacitySelect, () => {
-        return capacitySelect.value !== value ? false : true;
-      }, '1 комната только для 1 гостя');
-        break;
+      pristine.addValidator(capacitySelect, () => capacitySelect.value !== value ? false : true,
+      '1 комната только для 1 гостя');
+      break;
     }
 
     case '2': {
       const pristine = new Pristine(form, pristineConfig);
       capacitySelect.querySelector('option[value="2"]').setAttribute('selected', 'selected');
-      pristine.addValidator(capacitySelect, () => {
-        return capacitySelect.value > value || capacitySelect.value === '0' ? false : true;
-      }, '2 комнаты только для 1 или 2 гостей');
+      pristine.addValidator(capacitySelect, () => capacitySelect.value > value || capacitySelect.value === '0' ? false : true,
+      '2 комнаты только для 1 или 2 гостей');
       break;
     }
 
     case '3': {
       capacitySelect.querySelector('option[value="3"]').setAttribute('selected', 'selected');
       const pristine = new Pristine(form, pristineConfig);
-      pristine.addValidator(capacitySelect, () => {
-        return capacitySelect.value > value || capacitySelect.value === '0' ? false : true;
-      }, '3 комнаты только для 1, 2 или 3 гостей');
+      pristine.addValidator(capacitySelect, () => capacitySelect.value > value || capacitySelect.value === '0' ? false : true,
+      '3 комнаты только для 1, 2 или 3 гостей');
       break;
     }
 
     case '100': {
       capacitySelect.querySelector('option[value="0"]').setAttribute('selected', 'selected');
       const pristine = new Pristine(form, pristineConfig);
-      pristine.addValidator(capacitySelect, () => {
-        return capacitySelect !== '0' ? false : true;
-      }, '100 комнат не для гостей');
+      pristine.addValidator(capacitySelect, () => capacitySelect !== '0' ? false : true,
+      '100 комнат не для гостей');
       break;
     }
   }
@@ -104,42 +100,37 @@ const onTypeChange = () => {
     case 'bungalow': {
       priceInput.placeholder = '0';
       break;
-     break;
     }
 
     case 'flat': {
       priceInput.placeholder = '1000';
       const pristine = new Pristine(form, pristineConfig);
-      pristine.addValidator(priceInput, () => {
-        return priceInput.value * 1 < 1000 ? false : true;
-      }, 'Цена не может быть ниже 1000');
+      pristine.addValidator(priceInput, () => priceInput.value * 1 < 1000 ? false : true,
+      'Цена не может быть ниже 1000');
       break;
     }
 
     case 'hotel': {
       priceInput.placeholder = '3000';
       const pristine = new Pristine(form, pristineConfig);
-      pristine.addValidator(priceInput, () => {
-        return priceInput.value * 1 < 3000 ? false : true;
-      }, 'Цена не может быть ниже 3000');
+      pristine.addValidator(priceInput, () => priceInput.value * 1 < 3000 ? false : true,
+      'Цена не может быть ниже 3000');
       break;
     }
 
     case 'house': {
       priceInput.placeholder = '5000';
       const pristine = new Pristine(form, pristineConfig);
-      pristine.addValidator(priceInput, () => {
-        return priceInput.value * 1 < 5000 ? false : true;
-      }, 'Цена не может быть ниже 5000');
+      pristine.addValidator(priceInput, () => priceInput.value * 1 < 5000 ? false : true,
+      'Цена не может быть ниже 5000');
       break;
     }
 
     case 'palace': {
       priceInput.placeholder = '10000';
       const pristine = new Pristine(form, pristineConfig);
-      pristine.addValidator(priceInput, () => {
-        return priceInput.value * 1 < 10000 ? false : true;
-      }, 'Цена не может быть ниже 10000');
+      pristine.addValidator(priceInput, () => priceInput.value * 1 < 10000 ? false : true,
+      'Цена не может быть ниже 10000');
       break;
     }
   }
@@ -150,10 +141,10 @@ const timeOutSelect = form.querySelector('#timeout');
 
 const onInTimeChange = () => {
   timeOutSelect.value = timeInSelect.value;
-}
+};
 
 const onOutTimeChange = () => {
   timeInSelect.value = timeOutSelect.value;
-}
+};
 
 export {disableForm, enableForm, onRoomChange, onTypeChange, onInTimeChange, onOutTimeChange, adFormValidator};
